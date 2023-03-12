a5efee0912f7272283772b15085a17a391851580
Koczkás Péter <petikoczkas@hotmail.com>
2023-03-12 14:05:11 +0100

    Modify the trailing icon in UserCard

--- app/src/main/java/hu/bme/aut/bethere/ui/view/card/UserCard.kt
+++ app/src/main/java/hu/bme/aut/bethere/ui/view/card/UserCard.kt
@@ -3,7 +3,10 @@ package hu.bme.aut.bethere.ui.view.card
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,8 +22,8 @@ import hu.bme.aut.bethere.ui.theme.beThereTypography
 fun UserCard(
     text: String,
     onClick: () -> Unit,
-    addButtonOnClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
 ) {
     Card(
         modifier = modifier
@@ -48,9 +51,7 @@ fun UserCard(
                 )
                 Text(text = text, style = MaterialTheme.beThereTypography.cardTextStyle)
             }
-            IconButton(onClick = { addButtonOnClick() }) {
-                Icon(painter = painterResource(id = R.drawable.ic_add), contentDescription = null)
-            }
+            content()
         }
     }
 }
