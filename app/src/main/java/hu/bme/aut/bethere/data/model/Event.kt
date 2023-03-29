b0ad88b371e7dded25ef81b3e04288a18fb7e0d9
Koczkás Péter <petikoczkas@hotmail.com>
2023-03-29 20:02:57 +0200

    Update Event model

--- app/src/main/java/hu/bme/aut/bethere/data/model/Event.kt
+++ app/src/main/java/hu/bme/aut/bethere/data/model/Event.kt
@@ -3,10 +3,11 @@ package hu.bme.aut.bethere.data.model
 import android.os.Parcelable
 import com.google.firebase.Timestamp
 import kotlinx.parcelize.Parcelize
+import java.util.*
 
 @Parcelize
 data class Event(
-    val id: String = "",
+    val id: String = UUID.randomUUID().toString(),
     val name: String = "",
     val date: Timestamp = Timestamp.now(),
     val location: String = "",
