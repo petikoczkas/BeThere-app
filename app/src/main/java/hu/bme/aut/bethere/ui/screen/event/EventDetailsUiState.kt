e264f37f5bb20d0c67c4bd287bb4324f7f0bc701
Koczkás Péter <petikoczkas@hotmail.com>
2023-04-01 17:33:43 +0200

    Rework EventDetailsUiState

--- app/src/main/java/hu/bme/aut/bethere/ui/screen/event/EventDetailsUiState.kt
+++ app/src/main/java/hu/bme/aut/bethere/ui/screen/event/EventDetailsUiState.kt
@@ -5,12 +5,13 @@ import com.google.firebase.Timestamp
 sealed class EventDetailsUiState {
 
     data class EventDetailsLoaded(
+        val eventId: String,
         val eventName: String,
         val eventDate: Timestamp,
         val eventLocation: String,
     ) : EventDetailsUiState()
 
-    object EventDetails : EventDetailsUiState()
+    object EventDetailsInit : EventDetailsUiState()
 
     object EventDetailsSaved : EventDetailsUiState()
 }
