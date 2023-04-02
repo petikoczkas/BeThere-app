eab44f496afe8ced2080e0e563f1d90b68bf37b5
Koczkás Péter <petikoczkas@hotmail.com>
2023-04-02 19:42:17 +0200

    Extend constants list

--- app/src/main/java/hu/bme/aut/bethere/utils/Constants.kt
+++ app/src/main/java/hu/bme/aut/bethere/utils/Constants.kt
@@ -3,6 +3,7 @@ package hu.bme.aut.bethere.utils
 object Constants {
     const val USER_COLLECTION = "user"
     const val EVENT_COLLECTION = "event"
+    const val PICTURE_FOLDER = "profilePicture"
     const val NAME_PROPERTY = "name"
 
     val eventMembers = mutableListOf<String>()
