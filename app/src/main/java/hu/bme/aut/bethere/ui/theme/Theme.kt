000eea8ca14ad58087cc99b8fa7466a7f83e43f6
Koczkás Péter <petikoczkas@hotmail.com>
2023-04-03 20:05:29 +0200

    Implement DarkTheme into Theme.kt

--- app/src/main/java/hu/bme/aut/bethere/ui/theme/Theme.kt
+++ app/src/main/java/hu/bme/aut/bethere/ui/theme/Theme.kt
@@ -1,39 +1,22 @@
 package hu.bme.aut.bethere.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 
-/*private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)*/
-
 @Composable
-fun BeThereTheme(content: @Composable () -> Unit) {
+fun BeThereTheme(
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     CompositionLocalProvider(
         LocalColors provides BeThereColors(),
         LocalTypography provides BeThereTypography(),
         LocalDimens provides BeThereDimens()
     ) {
         MaterialTheme(
-            colors = LocalColors.current.colors,
+            colors = if (isDarkTheme) LocalColors.current.darkColors else LocalColors.current.lightColors,
             typography = Typography,
             shapes = Shapes,
             content = content
