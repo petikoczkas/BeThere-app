c21f2e8a449643eaceaea906268a746b34586e30
Koczkás Péter <petikoczkas@hotmail.com>
2023-04-04 20:29:28 +0200

    Update Dimens.kt

--- app/src/main/java/hu/bme/aut/bethere/ui/theme/Dimens.kt
+++ app/src/main/java/hu/bme/aut/bethere/ui/theme/Dimens.kt
@@ -9,6 +9,8 @@ import androidx.compose.ui.unit.dp
 
 data class BeThereDimens(
     val minButtonHeight: Dp = 52.dp,
+    val minBeThereTextFieldHeight: Dp = 60.dp,
+
     val dividerThickness: Dp = 1.dp,
 
     val gapNone: Dp = 0.dp,
@@ -22,16 +24,18 @@ data class BeThereDimens(
     val gapVeryVeryLarge: Dp = 56.dp,
     val gapExtraLarge: Dp = 64.dp,
 
-    val minBeThereTextFieldHeight: Dp = 56.dp,
     val minSearchFieldHeight: Dp = 40.dp,
     val minUserCardHeight: Dp = 62.dp,
     val minEventCardHeight: Dp = 70.dp,
+
+    val textFieldFocusedBorderThickness: Dp = 3.dp,
+    val textFieldUnfocusedBorderThickness: Dp = 2.dp,
+    val textFieldCornerSize: Dp = 12.dp,
 
     val userBoxHeight: Dp = 252.dp,
 
     val primaryButtonCornerSize: Dp = 12.dp,
     val primaryButtonBorderSize: Dp = 2.dp,
-    val textFieldCornerSize: Dp = 12.dp,
     val cardCornerSize: Dp = 12.dp,
 
     val circularProgressIndicatorSize: Dp = 80.dp,
