ab588dfd3dc97d00014970658d29426bfd502bc4
Koczkás Péter <petikoczkas@hotmail.com>
2023-04-09 18:43:50 +0200

    Rework UserCard

--- app/src/main/java/hu/bme/aut/bethere/ui/view/card/UserCard.kt
+++ app/src/main/java/hu/bme/aut/bethere/ui/view/card/UserCard.kt
@@ -1,27 +1,21 @@
 package hu.bme.aut.bethere.ui.view.card
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import hu.bme.aut.bethere.R
-import hu.bme.aut.bethere.ui.theme.beThereColors
 import hu.bme.aut.bethere.ui.theme.beThereDimens
 import hu.bme.aut.bethere.ui.theme.beThereTypography
 
 @Composable
 fun UserCard(
     text: String,
-    onClick: () -> Unit,
+    photo: String,
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
@@ -29,9 +23,8 @@ fun UserCard(
         modifier = modifier
             .fillMaxWidth()
             .clip(RoundedCornerShape(MaterialTheme.beThereDimens.cardCornerSize))
-            .heightIn(MaterialTheme.beThereDimens.minUserCardHeight)
-            .clickable { onClick() },
-        backgroundColor = MaterialTheme.beThereColors.gray,
+            .heightIn(MaterialTheme.beThereDimens.minUserCardHeight),
+        backgroundColor = MaterialTheme.colors.secondary,
         shape = RoundedCornerShape(MaterialTheme.beThereDimens.cardCornerSize)
     ) {
         Row(
@@ -42,12 +35,11 @@ fun UserCard(
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_account_circle),
-                    contentDescription = null,
+                ProfilePicture(
+                    photo = photo,
                     modifier = Modifier
-                        .size(40.dp)
                         .padding(end = MaterialTheme.beThereDimens.gapMedium)
+                        .size(MaterialTheme.beThereDimens.userCardImageSize)
                 )
                 Text(text = text, style = MaterialTheme.beThereTypography.cardTextStyle)
             }
