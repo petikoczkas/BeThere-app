ba9abdcea9282dae82951756e83e6adfcbe6c6e6
Koczkás Péter <petikoczkas@hotmail.com>
2023-04-09 18:20:09 +0200

    Update Dimens

--- app/src/main/java/hu/bme/aut/bethere/ui/theme/Dimens.kt
+++ app/src/main/java/hu/bme/aut/bethere/ui/theme/Dimens.kt
@@ -25,18 +25,24 @@ data class BeThereDimens(
     val gapExtraLarge: Dp = 64.dp,
 
     val minSearchFieldHeight: Dp = 40.dp,
-    val minUserCardHeight: Dp = 62.dp,
-    val minEventCardHeight: Dp = 70.dp,
+    val minUserCardHeight: Dp = 64.dp,
+    val userCardImageSize: Dp = 36.dp,
+    val minEventCardHeight: Dp = 64.dp,
 
     val textFieldFocusedBorderThickness: Dp = 3.dp,
     val textFieldUnfocusedBorderThickness: Dp = 2.dp,
     val textFieldCornerSize: Dp = 12.dp,
 
     val userBoxHeight: Dp = 252.dp,
+    val settingsImageSize: Dp = 200.dp,
 
     val primaryButtonCornerSize: Dp = 12.dp,
     val primaryButtonBorderSize: Dp = 2.dp,
+
     val cardCornerSize: Dp = 12.dp,
+    val messageCardSize: Dp = 340.dp,
+    val messageCardCornerSize: Dp = 16.dp,
+    val messageCardImageSize: Dp = 20.dp,
 
     val circularProgressIndicatorSize: Dp = 80.dp,
 
